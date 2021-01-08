53d96e218838efcee3c0dbc2db6454e68edef9a1
mazhihong_v <mazhihong_v@didichuxing.com>
2021-01-08 13:20:49 +0800

    添加 添加then函数
    then函数接收两个非必传函数；

--- myPromise.js
+++ myPromise.js
@@ -2,19 +2,53 @@ class MP {
     static PENDING = 'pending';
     static FULFILLED = 'fulfilled';
     static REJECTED = 'rejected';
+
     constructor(executor) {
         this.value = '';
-        this.reason = '';
         this.status = MP.PENDING;
-        executor(this.resolve.bind(this), this.reject.bind(this))
+        this.callback = [];
+        try {
+            executor( this.resolve.bind( this ), this.reject.bind( this ) )
+        }catch ( e ) {
+            this.reject(e)
+        }
+        // return new MP((onResolve, onReject)=>{
+        //     this.callback.push({
+        //         onResolve,
+        //         onReject
+        //     })
+        // })
     }
-    resolve(value){
-        this.value = value;
-        this.status = MP.FULFILLED;
+
+    resolve(value) {
+        if ( this.status === MP.PENDING ) {
+            this.value = value;
+            this.status = MP.FULFILLED;
+        }
     }
-    reject(reason){
-        this.reason = reason;
-        this.status = MP.REJECTED;
+
+    reject(reason) {
+        if ( this.status === MP.PENDING ) {
+            this.value = reason;
+            this.status = MP.REJECTED;
+        }
+    }
+    then(resolved, rejected){
+        // 如果resolve 和 rejected不存在
+        if (typeof resolved !== 'function') {
+            resolved = () => {}
+        }
+        if (typeof rejected !== 'function') {
+            rejected = () => {}
+        }
+        // 当前promise为成功则执行 resolved
+        if (this.status === MP.FULFILLED) {
+            resolved(this.value);
+        }
+        // 当前promise为rejected则执行 rejected
+        if(this.status === MP.REJECTED){
+            rejected(this.value)
+        }
     }
 }
 
